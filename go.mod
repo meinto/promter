d59f9eef94bb10f55c1b75ef7982fa92b376a404
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2019-03-06 23:47:58 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/meinto/cobra-utils
+
+require (
+	github.com/manifoldco/promptui v0.3.2
+	github.com/pkg/errors v0.8.1
+)
---
+++ promt.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/manifoldco/promptui"
+	"github.com/pkg/errors"
+)
+
+func PromptSelect(label string, options []string) (int, string, error) {
+	prompt := promptui.Select{
+		Label: label,
+		Items: options,
+	}
+	index, _, err := prompt.Run()
+	return index, options[index], errors.Wrap(err, "error running select promt")
+}
+
+func PromptOptionalText(label string) (string, error) {
+	getValue := promptui.Prompt{
+		Label: label,
+	}
+	value, err := getValue.Run()
+	return value, errors.Wrap(err, "error running optional text promt")
+}
