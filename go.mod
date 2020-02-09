985fe78c194e22bc39c09db5025e74985930db93
Tobias Meinhardt <tobias.meinhardt89@gmail.com>
2020-02-09 16:01:51 +0100

    Version 2

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/meinto/cobra-utils
 
 require (
+	github.com/imdario/mergo v0.3.8 // indirect
 	github.com/manifoldco/promptui v0.3.2
 	github.com/pkg/errors v0.8.1
 )
--- promt.go
+++ promt.go
@@ -1,23 +1,155 @@
 package cobraUtils
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/imdario/mergo"
 	"github.com/manifoldco/promptui"
-	"github.com/pkg/errors"
 )
 
-func PromptSelect(label string, options []string) (int, string, error) {
+type PromterOptions struct {
+	handleRetries bool
+}
+
+var prompterDefaultOptions = PromterOptions{true}
+
+func MergeOptions(options ...PromterOptions) PromterOptions {
+	o := prompterDefaultOptions
+	for _, option := range options {
+		mergo.Merge(&o, option, mergo.WithOverride)
+	}
+	return o
+}
+
+type Promter interface {
+	YesNo(label string) (index int, selection string, err error)
+	Select(label string, options []string) (index int, selection string, err error)
+	Text(label string, options ...PromterOptions) (input string, err error)
+	OptionalText(label string, options ...PromterOptions) (input string, err error)
+	URLWithDefault(label, defaultValue string, options ...PromterOptions) (url string, err error)
+	URL(label string, options ...PromterOptions) (url string, err error)
+}
+
+type promter struct {
+	retrys int
+}
+
+func NewPromter() Promter {
+	return &promter{0}
+}
+
+func (p *promter) HandleRetries(err error, cb func(), options ...PromterOptions) {
+	o := MergeOptions(options...)
+	if o.handleRetries {
+		if err == nil {
+			p.retrys = 0
+		}
+		p.retrys++
+		if err != nil && p.retrys <= 3 {
+			cb()
+		}
+	}
+}
+
+func (p *promter) YesNo(label string) (index int, selection string, err error) {
+	return p.Select(label, []string{"Yes", "No"})
+}
+
+func (p *promter) Select(label string, options []string) (index int, selection string, err error) {
 	prompt := promptui.Select{
 		Label: label,
 		Items: options,
 	}
-	index, _, err := prompt.Run()
-	return index, options[index], errors.Wrap(err, "error running select promt")
+	index, _, err = prompt.Run()
+	return index, options[index], err
 }
 
-func PromptOptionalText(label string) (string, error) {
+func (p *promter) Text(label string, options ...PromterOptions) (input string, err error) {
+	defer func() {
+		p.HandleRetries(err, func() {
+			p.Text(label, options...)
+		}, options...)
+	}()
+
+	validate := func(input string) error {
+		if strings.TrimSpace(input) == "" {
+			return fmt.Errorf("please provide a text")
+		}
+		return nil
+	}
+
+	notEmptyText := promptui.Prompt{
+		Label:    label,
+		Validate: validate,
+	}
+
+	notEmpty, err := notEmptyText.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.ToLower(notEmpty), nil
+}
+
+func (p *promter) OptionalText(label string, options ...PromterOptions) (input string, err error) {
+	defer func() {
+		p.HandleRetries(err, func() {
+			p.OptionalText(label, options...)
+		}, options...)
+	}()
+
 	getValue := promptui.Prompt{
 		Label: label,
 	}
 	value, err := getValue.Run()
-	return value, errors.Wrap(err, "error running optional text promt")
+	return value, err
+}
+
+func (p *promter) URLWithDefault(label, defaultValue string, options ...PromterOptions) (url string, err error) {
+	defer func() {
+		p.HandleRetries(err, func() {
+			p.URLWithDefault(label, defaultValue, options...)
+		}, options...)
+	}()
+
+	url, err = p.URL(fmt.Sprintf(label, defaultValue), PromterOptions{false})
+	if err != nil {
+		return url, err
+	}
+
+	if url == "" {
+		url = defaultValue
+	}
+
+	return strings.ToLower(url), nil
+}
+
+func (p *promter) URL(label string, options ...PromterOptions) (url string, err error) {
+	defer func() {
+		p.HandleRetries(err, func() {
+			p.URL(label, options...)
+		}, options...)
+	}()
+
+	validate := func(input string) error {
+		if input != "" &&
+			!strings.HasPrefix(input, "http://") &&
+			!strings.HasPrefix(input, "https://") {
+			return fmt.Errorf("please enter a valid url")
+		}
+		return nil
+	}
+
+	getURL := promptui.Prompt{
+		Label:    label,
+		Validate: validate,
+	}
+
+	url, err = getURL.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return strings.ToLower(url), nil
 }
